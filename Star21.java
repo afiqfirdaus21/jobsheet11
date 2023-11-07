edb25ebb090324f61d3657bed0c6f57d3cc05ef6
afiqfirdaus21 <144087118+afiqfirdaus21@users.noreply.github.com>
2023-11-07 15:09:28 +0700

    Add files via upload

---
+++ Star21.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Star21 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan Nilai N = ");
+        int N = sc.nextInt();
+
+        for (int i = 0; i <= N; i++) {
+            System.out.print("*");
+            
+        }
+    }
+    }
